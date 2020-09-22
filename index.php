bb391edc08c7c403c9bdabfe34e7fe2369747bdb
NotFerCam <davidfercam69@gmail.com>
2020-09-22 20:12:25 +0200

    miPrimerCommit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    $a = 12;
+    $b = "9.99";
+    $c = "ejemplo de variables PHP";
+
+    echo $a."<br>".$b."<br>".$c;
+    $miVariable = "prueba git merge";
+    $otra = 36;
+?>
+</body>
+</html>
